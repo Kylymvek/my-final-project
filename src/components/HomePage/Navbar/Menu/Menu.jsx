90787fb15d3f9dc5642ec8c1aeb356cac68b3634
Kylymvek <kylymbek.muratov.00@gmail.com>
2022-11-17 23:24:31 +0600

    commit 5

---
+++ src/components/HomePage/Navbar/Menu/Menu.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Menu.css";
+const Menu = ({ header, items, active, setActive }) => {
+  return (
+    <div className={active ? "menu active" : "menu"}>
+      <div className="blur" />
+      <div className="menu__content">
+        <div className="menu__header">{header}</div>
+        <ul className="menu__li">
+          {items.map(item => (
+            <li>
+              <a href={item.href}>{item.value}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
